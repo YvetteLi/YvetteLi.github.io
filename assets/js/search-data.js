1cb6799c51d49c0f48952a33bd3aec8260a6934b
YvetteLi <y938li@edu.uwaterloo.ca>
2025-12-02 20:26:22 +0000

    Deploying to gh-pages from @ YvetteLi/YvetteLi.github.io@ec2a370b152f905b4ba2c24a467808549ed84653 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,21 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "post-a-post-with-plotly-js",
+  },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
